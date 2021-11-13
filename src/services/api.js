9889492b3d13432d37bb049c108f927cb69df63b
Hugo Mesquita <hugo.web2@gmail.com>
2021-11-13 11:43:55 -0400

    up

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 //criando base url
-const url = 'http://192.168.64.2/jungle/api/app';
-//const url = 'https://amazonjunglepalace.com.br/api/app';
+//const url = 'http://192.168.64.2/jungle/api/app';
+const url = 'https://amazonjunglepalace.com.br/api/app';
 const api =  axios.create({
 	baseURL: url
 });
